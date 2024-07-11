58b07efcbed6ec413b153df7a2326db205da4cf4
JBjeff <boettcher-jeffrey@t-online.de>
2024-07-11 13:01:08 +0200

    sql

---
+++ target/classes/data.sql
@@ -0,0 +1,17 @@
+insert into games (id,username, description, done, points,success)
+values (10001,'Admin', 'Quiz', false, 0, false);
+
+insert into games (id,username, description, done, points,success)
+values (10002,'Admin' ,'Muell sortieren', false, 0, false );
+
+insert into games (id,username, description, done, points,success)
+values (10003,'Admin', 'Recyclebar oder nicht recyclebar', false, 0, false );
+
+insert into games (id,username, description, done, points,success)
+values (10004,'Admin', 'Memory', false, 0, false );
+
+insert into games (id,username, description, done, points,success)
+values (10005,'Admin', 'Müll Komponenten Sortieren', false, 0, false );
+
+insert into games (id,username, description, done, points,success)
+values (10006,'Admin', 'Müll Sortieren in einer 3D umgebung', false, 0, false );
---
+++ target/classes/quiz.sql
@@ -0,0 +1,5 @@
+
+
+
+INSERT INTO quizgame (description, done, points, success, user_id, quizQuestions, correctAnswers)
+VALUES ('Quiz Spiel', FALSE, 0, false, 1, 10, 8);
